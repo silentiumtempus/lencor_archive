b0badbdd2cef67536616737ace57bab6d93790b2
Artem Pimenov <APimenov@domain.corp>
2017-10-06 11:42:31 +0300

    Fixed ArchiveEntryRepository bug causing last entry update data no loading

--- src/AppBundle/Repository/ArchiveEntryRepository.php
+++ src/AppBundle/Repository/ArchiveEntryRepository.php
@@ -3,13 +3,24 @@
 namespace AppBundle\Repository;
 
 
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 
 class ArchiveEntryRepository extends EntityRepository
 {
+    protected $em;
+    protected $queryBuilder;
+
+    public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class)
+    {
+        parent::__construct($em, $class);
+        $this->em = $em;
+        $this->queryBuilder = $this->em->createQueryBuilder();
+    }
+
     public function getUpdateInfoByEntry(int $entryId)
     {
-        return $this->createQueryBuilder('g')
+        return $this->queryBuilder
             ->select('en.lastModified', 'us.usernameCanonical')
             ->from('AppBundle:ArchiveEntryEntity', 'en')
             ->leftJoin('AppBundle:User', 'us', 'with', 'en.modifiedByUserId = us.id')
@@ -20,7 +31,7 @@ class ArchiveEntryRepository extends EntityRepository
 
     public function getUpdateInfoByFolder(int $entryId)
     {
-        return $this->createQueryBuilder('g')
+        return $this->queryBuilder
             ->select('en.lastModified', 'us.usernameCanonical')
             ->from('AppBundle:ArchiveEntryEntity', 'en')
             ->leftJoin('AppBundle:User', 'us', \Doctrine\ORM\Query\Expr\Join::WITH, 'en.modifiedByUserId = us.id')
