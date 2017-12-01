525ea12289930548df86d2d81167cc09c5ec9f89
silentiumtempus <f0rt@naebal.ru>
2017-12-01 16:23:36 +0300

    Initial changes to prepare for new log finder&viewer functionality

--- src/AppBundle/Controller/LogManagerController.php
+++ src/AppBundle/Controller/LogManagerController.php
@@ -4,7 +4,10 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\ArchiveEntryEntity;
 use AppBundle\Form\ArchiveEntryLogSearchForm;
+use AppBundle\Service\FolderService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,12 +17,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class LogManagerController extends Controller
 {
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return Response
      * @Route("/logging/", name="logging")
      */
-    public function logManagerIndex()
+    public function logManagerIndex(Request $request, FolderService $folderService)
     {
         $logSearchForm = $this->createForm(ArchiveEntryLogSearchForm::class, new ArchiveEntryEntity());
+        $logSearchForm->handleRequest($request);
+        if ($logSearchForm->isSubmitted() && $logSearchForm->isValid() && $request->isMethod('POST'))
+        {
+
+        }
+
         return $this->render('lencor/admin/archive/logging_manager/index.html.twig', array('logSearchForm' => $logSearchForm->createView()));
     }
 }
--- src/AppBundle/Form/ArchiveEntryLogSearchForm.php
+++ src/AppBundle/Form/ArchiveEntryLogSearchForm.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Form;
 
-
 use AppBundle\Entity\ArchiveEntryEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
