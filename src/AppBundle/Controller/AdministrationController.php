1384d33690d973d33b382f86963224181134cff4
silentiumtempus <f0rt@naebal.ru>
2017-12-14 16:47:14 +0300

    New skeleton controllers and templates for admin, stats and system menu options

---
+++ src/AppBundle/Controller/AdministrationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdministrationController
+ * @package AppBundle\Controller
+ */
+class AdministrationController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/admin/", name="admin")
+     */
+    public function statsIndex(Request $request)
+    {
+        return $this->render(':lencor/admin/archive/administration:index.html.twig');
+    }
+}
---
+++ src/AppBundle/Controller/SystemManagerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SystemManagerController
+ * @package AppBundle\Controller
+ */
+class SystemManagerController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/system/", name="system")
+     */
+    public function statsIndex(Request $request)
+    {
+        return $this->render(':lencor/admin/archive/system_manager:index.html.twig');
+    }
+}
