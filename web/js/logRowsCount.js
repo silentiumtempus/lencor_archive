aa923b91c4a7c8fe9e649e6b5c5e9458963067f9
silentiumtempus <f0rt@naebal.ru>
2017-12-20 16:00:14 +0300

    LogRowsCount.js file cleanup for previous commit

--- web/js/logRowsCount.js
+++ web/js/logRowsCount.js
@@ -2,19 +2,14 @@ $(document).ready(function () {
     if (!window.jQuery) {
     } else {
         let $logsRowsCountForm = $('#logs_rows_count_form');
-        let $path = $("#log-search").attr("data-path");
         $logsRowsCountForm.on("submit", function (event) {
             event.preventDefault();
-            let rowsCount = $('#log_rows_count_form_rowsCount').val();
-            let entryId = $('#log_rows_count_form_entryId').attr('value');
-            let file = $('#log_rows_count_form_file').attr('value');
             let $logsRowsCountFormSerialized = $logsRowsCountForm.serialize();
             $.ajax({
                 url: "logging/open-file",
                 method: "POST",
                 data: $logsRowsCountFormSerialized,
                 success: function (response) {
-                    //alert('OK');
                     $('#logfile-content').html(response);
                 }
             });
