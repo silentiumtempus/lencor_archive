561c7171c460e3b553146870a7ab07b0757d2ac5
Artem Pimenovgit <f0rt@live.com>
2018-02-10 10:05:30 +0300

    New Security component for future Kerberos authorization

---
+++ src/AppBundle/Security/UserProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+/**
+ * Class UserProvider
+ * @package AppBundle\Security
+ */
+class UserProvider implements UserProviderInterface
+{
+    /**
+     * @param string $username
+     * @return User
+     */
+    public function loadUserByUsername($username)
+    {
+
+        return new User(null, null, null, null, array());
+    }
+
+    /**
+     * @param UserInterface $user
+     * @return User
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Непредвиденная ошибка : Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * @param string $class
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return User::class === $class;
+    }
+}
