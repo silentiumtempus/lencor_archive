aa15c90be3119dda659eb6264ba5b580287dce45
silentiumtempus <f0rt@naebal.ru>
2018-04-17 12:25:37 +0300

    Menu js refactoring

--- assets/js/menuScript.js
+++ assets/js/menuScript.js
@@ -1,28 +1,9 @@
 ( function( $ ) {
 $( document ).ready(function() {
-$('#cssmenu').prepend('<div id="indicatorContainer"><div id="pIndicator"><div id="cIndicator"></div></div></div>');
-    let activeElement = $('#cssmenu>ul>li:first');
-
-    $('#cssmenu>ul>li').each(function() {
-        if ($(this).hasClass('active')) {
-            activeElement = $(this);
-        }
-    });
-
-
-	let posLeft = activeElement.position().left;
-	let elementWidth = activeElement.width();
-	posLeft = posLeft + elementWidth/2 -6;
-	if (activeElement.hasClass('has-sub')) {
-		posLeft -= 6;
-	}
-
-	$('#cssmenu #pIndicator').css('left', posLeft);
-	let element, leftPos, indicator = $('#cssmenu pIndicator');
-	
 	$("#cssmenu>ul>li").hover(function() {
-        element = $(this);
+        let element = $(this);
         let w = element.width();
+        let leftPos = null;
         if ($(this).hasClass('has-sub'))
         {
         	leftPos = element.position().left + w/2 - 12;
@@ -32,13 +13,9 @@ $('#cssmenu').prepend('<div id="indicatorContainer"><div id="pIndicator"><div id
         }
 
         $('#cssmenu #pIndicator').css('left', leftPos);
-    }
-    , function() {
-    	$('#cssmenu #pIndicator').css('left', posLeft);
     });
 
-	$('#cssmenu>ul').prepend('<li id="menu-button"><a>Menu</a></li>');
-	$( "#menu-button" ).click(function(){
+	$("#menu-button").click(function(){
     		if ($(this).parent().hasClass('open')) {
     			$(this).parent().removeClass('open');
     		}
