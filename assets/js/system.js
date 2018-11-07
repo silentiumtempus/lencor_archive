d8a31f0defd1699aa6b67d2e2a80ac6794bd3695
silentiumtempus <f0rt@naebal.ru>
2018-11-07 13:20:46 +0300

    Minor improvements for future System manager functionality

--- assets/js/system.js
+++ assets/js/system.js
@@ -1,14 +1,38 @@
 $(document).ready(function () {
     if (window.jQuery) {
+        $(document).on('click', '#info', function () {
+            loadData('info');
 
-        $(document).on("click", "#sys-info", loadData('sys-info'));
-        $(document).on("click", "#app-config", loadData('app-config'));
-        $(document).on("click", "#php-settings", loadData('php-settings'));
-        $(document).on("folder-permissions", loadData('folder-permissions'));
+            return false;
+        });
+        $(document).on('click', '#config-info', function () {
+            loadData('config-info');
+
+            return false;
+        });
+        $(document).on('click', '#php-config-info', function () {
+            loadData('php-config-info');
+
+            return false;
+        });
+        $(document).on('click', '#permissions-info', function () {
+            loadData('permissions-info');
+
+            return false;
+        });
 
         function loadData(target) {
+            $.ajax({
+                url: Routing.generate('system-' + target),
+                method: "POST",
+                data: null,
+                success: function (response) {
+                    $('#system-content').html(response);
+                    alert(response);
+                }
+            });
 
-            return null;
+            return false;
         }
 
     }
