4f05ba9d4f37705f488f20146751148f69c72118
silentiumtempus <f0rt@naebal.ru>
2018-11-08 12:46:10 +0300

    Fix for front-end common window scaling issue

--- assets/js/system.js
+++ assets/js/system.js
@@ -28,7 +28,6 @@ $(document).ready(function () {
                 data: null,
                 success: function (response) {
                     $('#system-content').html(response);
-                    alert(response);
                 }
             });
 
